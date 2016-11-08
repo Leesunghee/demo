6a14376b3e6c08547898ac63ccca1463ab0d997e
leesunghee <realwunz@gmail.com>
2016-11-08 18:20:17 +0900

    chapter2 spring boot starter and auto configuration practice

---
+++ src/main/java/com/example/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by hds on 2016. 11. 8..
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {//extends WebSecurityConfigurerAdapter {
+}
