832cebea3c4b37f4bdf1d75971ca4256126133f3
leesunghee <realwunz@gmail.com>
2016-11-15 18:26:02 +0900

    chapter2 spring boot starter and auto configuration practice

--- src/main/java/com/example/ReadingListController.java
+++ src/main/java/com/example/ReadingListController.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,7 +27,7 @@ public class ReadingListController {
     }
 
     @RequestMapping(method= RequestMethod.GET)
-    public String readersBooks(Reader reader, Model model) {
+    public String readersBooks(@ModelAttribute Reader reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
