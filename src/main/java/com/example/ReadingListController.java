a24703c535744209929866c473db82df45be4ac9
sunghee <realwunz@gmail.com>
2016-11-27 17:48:54 +0900

    add spring boot propeties

--- src/main/java/com/example/ReadingListController.java
+++ src/main/java/com/example/ReadingListController.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,15 +16,21 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon") //프로퍼티 주입
 public class ReadingListController {
 
     //private static final String reader = "craig";
 
     private ReadingListRepository readingListRepository;
+    private String associatedId;
 
     @Autowired
     public ReadingListController(ReadingListRepository readingListRepository) {
         this.readingListRepository = readingListRepository;
+    }
+
+    public void setAssociateId(String associateId) { //제휴 ID의 세터 메서드
+        this.associatedId = associateId;
     }
 
     @RequestMapping(method= RequestMethod.GET)
@@ -32,7 +39,10 @@ public class ReadingListController {
         if (readingList != null) {
             model.addAttribute("books", readingList);
             model.addAttribute("reader", reader);
+
+            model.addAttribute("amazonID", associatedId); //제휴 ID를 모델에 추가
         }
+        System.out.println("#################### hot swapping test ok! ##");
         return "readingList";
     }
 
